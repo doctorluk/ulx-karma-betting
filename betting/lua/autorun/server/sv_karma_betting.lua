a09760966f6f00c4bb85a5ab9be0e9f26e4b81c2
Lukas Monka <lukgit@gmail.com>
2016-10-07 22:07:40 +0200

    Show bets only when a certain amount of corpses have been found

--- betting/lua/autorun/server/sv_karma_betting.lua
+++ betting/lua/autorun/server/sv_karma_betting.lua
@@ -8,7 +8,9 @@ if SERVER then
 	
 	karmabet_bet_total_t = 0
 	karmabet_bet_total_i = 0
+	karmabet_corpses_found = 0
 	karmabet_tbl_betters = {}
+	karmabet_tbl_betters_hidden = {}
 	karmabet_tbl_results = {}
 	karmabet_winner = ""
 	
@@ -35,7 +37,7 @@ if SERVER then
 	end
 	
 	-- Show spectating/dead players that someone has placed a bet
-	function karmabet_echoBetPlacement( ply, amount, target )
+	function karmabet_echoBetPlacement( ply, amount, target, puthidden )
 	
 		local color = Color( 0, 255, 0, 255 )
 		if target == "innocent" then
@@ -45,12 +47,18 @@ if SERVER then
 			color = Color( 255, 0, 0, 255 )
 		end
 		
+		local hidden = " "
+		
+		if puthidden then
+			hidden = "[VERSTECKT] "
+		end
+		
 		for _, player in ipairs( player.GetHumans() ) do
 			if not player:Alive() or player:IsSpec() then
 				ULib.tsayColor( player, true,
 				Color( 50, 50, 50, 255 ), "[", 
 				Color( 190, 40, 40, 255 ), "Karmabet",
-				Color( 50, 50, 50, 255 ), "] ", 
+				Color( 50, 50, 50, 255 ), "]" .. hidden, 
 				Color( 255, 255, 0, 0 ), ply:Nick(),
 				Color( 60, 90, 100, 255 ), " wettet ",
 				Color( 190, 40, 40, 255), amount .. "",	
@@ -63,6 +71,8 @@ if SERVER then
 	
 	-- Big check whether or not someone can bet
 	function karmabet_canBet( calling_ply, amount, target )
+	
+		local puthidden = false
 	
 		if not calling_ply then return false end
 	
@@ -117,8 +127,19 @@ if SERVER then
 			karmabet_reportNotice( calling_ply, "Dein Karma ist low! Es konnte nur " .. amount .. " Karma gesetzt werden!" )
 		end
 		
+		if not karmabet_enoughCorpsesFound() then
+			puthidden = true
+		end
+		
+		local tmptable = {}
+		
 		-- Check if player changed his mind about voting or limit is reached
-		local tmptable = karmabet_tbl_betters[calling_ply:SteamID()]
+		if puthidden then
+			tmptable = karmabet_tbl_betters_hidden[calling_ply:SteamID()]
+		else
+			tmptable = karmabet_tbl_betters[calling_ply:SteamID()]
+		end
+		
 		if tmptable then
 			local saved_amount = tmptable[1]
 			local saved_target = tmptable[2]
@@ -143,17 +164,26 @@ if SERVER then
 			
 			-- Update player's saved amount
 			local new_saved_amount = amount + saved_amount
-			karmabet_tbl_betters[calling_ply:SteamID()] = { new_saved_amount, target }
+			if puthidden then
+				karmabet_tbl_betters_hidden[calling_ply:SteamID()] = { new_saved_amount, target }
+			else
+				karmabet_tbl_betters[calling_ply:SteamID()] = { new_saved_amount, target }
+			end
 		end
 		
 		-- If all passes, start bet
-		karmabet_start( calling_ply, amount, target )
+		karmabet_start( calling_ply, amount, target, puthidden )
 		
 		return true
 	end
 	
+	function karmabet_enoughCorpsesFound()
+		print(karmabet_corpses_found .. " " .. KARMABET_MINIMUM_IDENTIFIED_BODIES .. "\n")
+		return karmabet_corpses_found >= KARMABET_MINIMUM_IDENTIFIED_BODIES
+	end
+	
 	-- Actually start the bet
-	function karmabet_start( calling_ply, amount, target )
+	function karmabet_start( calling_ply, amount, target, puthidden )
 	
 		-- Reduce player's karma by the amount he bet
 		calling_ply:SetLiveKarma( calling_ply:GetLiveKarma() - amount )
@@ -170,19 +200,47 @@ if SERVER then
 		end
 		
 		-- Add player to table of players who have placed a bet
-		if not karmabet_tbl_betters[calling_ply:SteamID()] then
-			karmabet_tbl_betters[calling_ply:SteamID()] = { amount, target }
+		if puthidden then
+			if not karmabet_tbl_betters_hidden[calling_ply:SteamID()] then
+				karmabet_tbl_betters_hidden[calling_ply:SteamID()] = { amount, target }
+			end
+		else
+			if not karmabet_tbl_betters[calling_ply:SteamID()] then
+				karmabet_tbl_betters[calling_ply:SteamID()] = { amount, target }
+			end
 		end
-		PrintTable( karmabet_tbl_betters )
+		-- PrintTable( karmabet_tbl_betters )
 		
-		karmabet_echoBetPlacement( calling_ply, amount, target )
-		karmabet_updateAllPlayers()
+		karmabet_echoBetPlacement( calling_ply, amount, target, puthidden )
+		
+		if not puthidden then karmabet_updateAllPlayers() end
 		
 	end
 	
+	-- Function called when finding a player
+	function karmabet_onBodyFound(ply, deadply, rag)
+		if ply and deadply then
+			karmabet_corpses_found = karmabet_corpses_found + 1
+			-- karmabet_reportNotice( nil, "[DEBUG] Found corpse of player " .. deadply:Nick() .. ", karmabet_corpses_found: " .. karmabet_corpses_found )
+		end
+		
+		if karmabet_enoughCorpsesFound() and table.Count( karmabet_tbl_betters_hidden ) > 0 then
+			for id, entry in pairs( karmabet_tbl_betters_hidden ) do
+				local ply = player.GetBySteamID( id )
+				if ply then -- See if he's actually on the server
+					karmabet_tbl_betters[id] = { entry[1], entry[2] }
+				end
+				
+			end
+			table.Empty( karmabet_tbl_betters_hidden )
+			karmabet_refresh()
+		end
+		
+	end
+	hook.Add( "TTTBodyFound", "karmabet_onBodyFound", karmabet_onBodyFound )
+	
 	-- Lazy refresh of running bets
 	function karmabet_refresh()
-	
 		local bet_i = 0
 		local bet_t = 0
 		
@@ -213,10 +271,13 @@ if SERVER then
 		
 	end
 	
+	-- Running on the start of a round and timing the betting end
 	function karmabet_timedBettingEnd()
 	
+		-- Reset everything to defaults
 		KARMABET_CAN_BET = true
 		KARMABET_HAS_RUN = false
+		karmabet_corpses_found = 0
 		table.Empty( karmabet_tbl_results )
 		karmabet_winner = ""
 		
@@ -249,7 +310,6 @@ if SERVER then
 	
 	-- Act after a round has ended
 	function karmabet_onRoundEnd( winning_team )
-	
 		if not KARMABET_HAS_RUN then
 		
 			KARMABET_HAS_RUN = true
@@ -267,6 +327,18 @@ if SERVER then
 			
 			local own_bet_winscale = (math.random(10, 25) / 100) -- between 10% and 25% of entered bet
 			local losers_bet_winscale = (math.random(5, 15) / 100) -- between 5% and 15% of bets from the losing team
+			
+			-- First we put our hidden entries into the normal database	if that hasn't happened before
+			for id, entry in pairs( karmabet_tbl_betters_hidden ) do
+			
+				local ply = player.GetBySteamID( id )
+				if ply then -- See if he's actually on the server
+					karmabet_tbl_betters[id] = { entry[1], entry[2] }
+				end
+				
+			end
+			
+			table.Empty( karmabet_tbl_betters_hidden )
 			
 			-- Winners get their bet, and a bonus depending on the amount of bets against them				
 			for id, entry in pairs( karmabet_tbl_betters ) do
@@ -301,13 +373,13 @@ if SERVER then
 					else
 					
 						ULib.tsayColor( nil, false,
-						Color( 50, 50, 50, 255 ), "[", 
-						Color( 190, 40, 40, 255), "Karmabet",
-						Color( 50, 50, 50, 255), "] ",
-						Color( 255, 255, 0, 0 ), ply:Nick(),
-						Color( 255, 0, 0, 255), " verliert ",
-						Color( 255, 255, 255, 255), amount .. "",
-						Color( 255, 0, 0, 255), " Karma!" )
+							Color( 50, 50, 50, 255 ), "[", 
+							Color( 190, 40, 40, 255), "Karmabet",
+							Color( 50, 50, 50, 255), "] ",
+							Color( 255, 255, 0, 0 ), ply:Nick(),
+							Color( 255, 0, 0, 255), " verliert ",
+							Color( 255, 255, 255, 255), amount .. "",
+							Color( 255, 0, 0, 255), " Karma!" )
 						
 						karmabet_tbl_results[id] = { amount, target }
 					end
@@ -327,6 +399,40 @@ if SERVER then
 		end
 	end
 	hook.Add( "TTTEndRound", "karmabet_onRoundEnd", karmabet_onRoundEnd )
+	
+	--[[
+	function karmabet_onBodyFound(ply, deadply, corpse)
+	
+		if KARMABET_DELAY_BET_ANNOUCEMENT and deadply and karmabet_corpses_found < KARMABET_MINIMUM_IDENTIFIED_BODIES then
+		
+			local refresh = false
+			
+			karmabet_corpses_found = karmabet_corpses_found + 1
+			
+			for id, entry in pairs( karmabet_tbl_betters_hidden ) do
+				if id == deadply:SteamID() then
+					karmabet_reportNotice( nil, deadply:Nick() .. "'s Wette wurde nachgetragen!" )
+					karmabet_tbl_betters[id] = { entry[1], entry[2] }
+					karmabet_tbl_betters_hidden[id] = nil
+					if not refresh then refresh = true end
+				end
+			end
+			
+			if refresh then karmabet_refresh() end
+			
+		elseif karmabet_corpses_found >= KARMABET_MINIMUM_IDENTIFIED_BODIES and #karmabet_tbl_betters_hidden > 0 then
+			for id, entry in pairs( karmabet_tbl_betters_hidden ) do
+					karmabet_reportNotice( nil, "Genug Leute tot, jetzt führen wir alle Wetten auf!" )
+					karmabet_tbl_betters[id] = { entry[1], entry[2] }
+					karmabet_tbl_betters_hidden[id] = nil
+			end
+			karmabet_refresh()
+		end
+		
+		karmabet_reportNotice( nil, ply:Nick() .. " found body of " .. deadply:Nick() )
+	end
+	hook.Add( "TTTBodyFound", "karmabet_onBodyFound", karmabet_onBodyFound )
+	]]--
 	
 	-- Send new player current bets
 	function karmabet_onPlayerConnect( ply )
--- betting/lua/autorun/sv_karma_betting_config.lua
+++ betting/lua/autorun/sv_karma_betting_config.lua
@@ -4,6 +4,8 @@
 
 KARMABET_USE_MYSQL = true -- Please refer to server/sv_karma_betting_mysql.lua for database configuration
 
+KARMABET_MINIMUM_IDENTIFIED_BODIES = 2
+
 KARMABET_MAXIMUM_KARMA = 200
 KARMABET_MINIMUM_KARMA = 10
 KARMABET_BET_TIME = 180
