225389302bbe6cd5b5a06de9c7fa6b66f905420d
Lukas Monka <lukgit@gmail.com>
2016-10-09 20:08:49 +0200

    Add configuration comments

--- betting/lua/autorun/sv_karma_betting_config.lua
+++ betting/lua/autorun/sv_karma_betting_config.lua
@@ -2,12 +2,24 @@
 -- http://steamcommunity.com/id/doctorluk/
 -- Version: 1.0
 
-KARMABET_USE_MYSQL = false -- Please refer to server/sv_karma_betting_mysql.lua for database configuration if set to true
+-- If set to false it will use the SQLite database within the server's installation to store the progress
+-- Set to true to use MySQL to save the karmabet progress
+-- Requires the mysqloo addon for Windows/Linux for GMod
+-- Refer to lua/autorun/server/sv_karma_betting_mysql.lua for configuration options
+KARMABET_USE_MYSQL = false
 
+-- Configure the minimum amount of identified corpses to show the total amount of placed bets
+-- publicly
 KARMABET_MINIMUM_IDENTIFIED_BODIES = 2
 
-KARMABET_MAXIMUM_KARMA = 200
+-- Minimum amount of karma to bet
 KARMABET_MINIMUM_KARMA = 10
+
+-- Maximum amount of karma to bet
+KARMABET_MAXIMUM_KARMA = 200
+
+-- Time to bet once a round has started
 KARMABET_BET_TIME = 180
 
+-- Minimum karma of a player to have to bet (bets exceeding their karma will be lowered to this minimum here)
 KARMABET_MINIMUM_LIVE_KARMA = 600
